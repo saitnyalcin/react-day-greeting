9c901c721affbedacd8cb828edc70cdf1ee358b9
steveyalcin <steven.yalcin@gmail.com>
2020-06-01 16:45:37 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Afternoon from './images/afternoon.png';
+import Default from './images/default.jpg';
+import Evening from './images/evening.png';
+import Morning from './images/morning.jpg';
+import Night from './images/night.jpg';
 
 function App() {
+  // set the current date and time object
+  var todayDate = new Date();
+  let timeNow = todayDate.getHours();
+  let dayToday, dayImage;
+
+  // set the if statemtent to specify the time of day
+  if (timeNow < 12) {
+    dayToday = 'morning';
+  } else if (timeNow > 12 && timeNow < 17) {
+    dayToday = 'afternoon';
+  } else if (timeNow > 17 && timeNow < 20) {
+    dayToday = 'evening';
+  } else {
+    dayToday = 'night';
+  }
+
+  // set the switch statement to set the portion of the date
+  switch (dayToday) {
+    case 'morning':
+      dayImage = Morning;
+      break;
+    case 'afternoon':
+      dayImage = Afternoon;
+      break;
+    case 'evening':
+      dayImage = Evening;
+      break;
+    case 'night':
+      dayImage = Night;
+      break;
+    default:
+      dayImage = Default;
+      break;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={dayImage} alt="" />
     </div>
   );
 }
